4bc319529dd3452a36317ec4f165ca5a94d121d9
idoheinemann <idohaineman@gmail.com>
2020-01-01 20:04:21 +0200

    added logging

--- vision_master.py
+++ vision_master.py
@@ -3,6 +3,7 @@ import gbrpi
 import numpy as np
 
 from algorithms import BaseAlgorithm
+from utils.logger import Logger
 
 TABLE_NAME = 'vision'
 TABLE_IP = '10.45.90.2'
@@ -11,8 +12,14 @@ SUCCESS_KEY = 'found'
 
 
 def main():
+    logger = Logger(__file__)
     conn = gbrpi.TableConn(ip=TABLE_IP, table_name=TABLE_NAME)
+    logger.log('initialized conn')
     camera = gbv.USBCamera(0, gbv.LIFECAM_3000.rotate_pitch(np.deg2rad(35)))
+    logger.log('initialized camera')
+
+
+    conn.set('algorithm', 'targets')
 
     all_algos = BaseAlgorithm.get_algorithms()
     possible_algos = {key: all_algos[key](OUTPUT_KEY, SUCCESS_KEY, conn) for key in all_algos}
